04f7ca61a2d269494b0546621170276277857e09
SamuelJPalacios <157061469+SamuelJPalacios@users.noreply.github.com>
2024-05-28 16:40:09 -0600

    Ahora si corregir caja de texto

--- front/chat.php
+++ front/chat.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="utf-8">
-<title>PomPom Chat</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
-<link rel="stylesheet" type="text/css" href="style.css">
+    <meta charset="utf-8">
+    <title>PomPom Chat</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 
 <?php
